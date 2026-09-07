e09e5d639be4bb0741b014abc66d7fde6528a092
schuckf <schuck.felix@googlemail.com>
2026-09-07 12:23:00 +0200

    added comments on daily transpiration deficit
    it is still unclear which method to use to get the daily vc_TranspirationDeficit (either from hourly vc_TranspirationDeficit_h or only using daily calculations similarly to default MONICA)

--- src/core/crop-module.cpp
+++ src/core/crop-module.cpp
@@ -1216,6 +1216,8 @@ void CropModule::step(double vw_MeanAirTemperature,
       // aggregation back to daily time step, mean
       vc_TranspirationDeficit = accumulate(hourly_TranspirationDeficit_day.begin(), hourly_TranspirationDeficit_day.end(), 0.) / hourly_TranspirationDeficit_day.size();
       // @ ToDo FS: Is mean good enough, or is a weighted mean (e.g. with hourly photosynthesis) required?
+      // @ ToDo FS: Or should we calculate the daily vc_TranspirationDeficit in a similar way as in CropModule::fc_CropWaterUptake(...)?
+      // vc_TranspirationDeficit = ... calculate daily vc_TranspirationDeficit from CropModule::fc_CropWaterUptake(...) without actually running CropModule::fc_CropWaterUptake(...) ...
 
       vc_KTkc = get<0>(vc_KTkc_vc_KTko(vw_MeanAirTemperature));                                             // FS: reaction speed factor with the (daily) mean temperature (=default daily MONICA)
       // vc_KTkc = accumulate(hourly_KTkc_day.begin(), hourly_KTkc_day.end(), 0.) / hourly_KTkc_day.size(); //     vs. mean of the (hourly) reaction speed factors
